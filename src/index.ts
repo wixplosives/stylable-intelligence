f3defa36883d1eb9534cc36334ecc7121511a6fd
Tal Harel <talh@wix.com>
2017-07-30 15:45:49 +0300

    Added provider exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export {activate} from './extension';
 console.log('activated');
+import Provider, {Completion, ExtendedResolver, ProviderRange, FsEntity} from './provider';
+export {Provider, Completion, ExtendedResolver, ProviderRange, FsEntity};
