3804e52d781302f4514a48e4a78cdb9ec7b22c3f
DoronTsur <doront@wix.com>
2017-08-13 12:05:53 +0300

    allow debugger #15

--- packages/client/src/extension.ts
+++ packages/client/src/extension.ts
@@ -6,14 +6,20 @@ import path = require('path');
 
 
 export function activate(context: ExtensionContext) {
+
     console.log('client lalala');
     let serverModule = context.asAbsolutePath(path.join('server', 'src', 'server.js'));
+    let debugOptions = { execArgv: ['--inspect'] };
 
     let serverOptions: ServerOptions = {
          run: { module: serverModule, transport: TransportKind.ipc },
-         debug: { module: serverModule, transport: TransportKind.ipc }
+         debug: { module: serverModule, transport: TransportKind.ipc, options:debugOptions, runtime:'node', args:['--inspect:6004'] }
 
     }
+    // let serverOptions: ServerOptions = {
+    //      run:  {command: 'node', args:[serverModule]},
+    //      debug:  {command: 'node', args:[serverModule]}
+    // }
     let clientOptions: LanguageClientOptions = {
         documentSelector: ['css'],
     }
