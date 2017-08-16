814e1ed030bca9c54a57392568e6771939c2136f
DoronTsur <doront@wix.com>
2017-08-16 13:46:03 +0300

    refactor after test pass

--- packages/client/test/completion.test.ts
+++ packages/client/test/completion.test.ts
@@ -1,25 +1,19 @@
 import * as assert from 'assert';
-import * as Bluebird from 'bluebird';
 import * as path from 'path';
 import * as vscode from 'vscode';
 
-function testCompletion(fileToTest: string, testCases: [vscode.Position, string[]][], extraFiles: string[] =[]) {
-    let casesPath = path.join(__dirname, '..', '..', 'test', 'cases', fileToTest)
-
-    let testDoc:vscode.TextDocument;
+function testCompletion(fileToTest: string, testCases: [vscode.Position, string[]][]) {
+    const casesPath = path.join(__dirname, '..', '..', 'test', 'cases', fileToTest)
+    const ext = vscode.extensions.getExtension('Wix.stylable-intelligence')
+    let testDoc:vscode.TextDocument
 
     return vscode.workspace.openTextDocument(casesPath)
-        .then((textDocument) => {
-            testDoc = textDocument;
-            return Promise.all(extraFiles.map(file => {
-                return vscode.workspace.openTextDocument
-            }))
-        }).then(()=> {
-            return vscode.window.showTextDocument(testDoc)
-        }).then(() => {
-            const ext = vscode.extensions.getExtension('Wix.stylable-intelligence')
-            return Bluebird.delay(500, ext!.activate)
-        }).then(() => {
+        .then((doc)=> {
+           testDoc = doc;
+           return vscode.window.showTextDocument(testDoc)
+        })
+        .then(() =>  ext!.activate())
+        .then(() => {
             let promises = testCases.map(([position, expected]) => {
                 return vscode.commands.executeCommand<vscode.CompletionList>('vscode.executeCompletionItemProvider', testDoc.uri, position)
                     .then(list => {
@@ -29,8 +23,9 @@ function testCompletion(fileToTest: string, testCases: [vscode.Position, string[
                                 assert.fail('', entry, 'missing expected item in competion list', '');
                             }
                         }
+                        return Promise.resolve()
                     })
-            });
+            })
             return Promise.all(promises);
         });
 }
@@ -49,7 +44,7 @@ suite("Extension Tests", () => {
         const testCases: [vscode.Position, string[]][] = [
             [new vscode.Position(10, 6), ['shmover', 'bover']]
         ];
-        return testCompletion('advanced-completion.css', testCases, ['advanced-dependency.css']);
+        return testCompletion('advanced-completion.css', testCases);
     });
 });
 
