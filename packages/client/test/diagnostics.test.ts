6a05ac4768744aa76b620c2735c2a5dd8a2aa9d6
DoronTsur <doront@wix.com>
2017-08-23 17:18:29 +0300

    e2e refactor

--- packages/client/test/diagnostics.test.ts
+++ packages/client/test/diagnostics.test.ts
@@ -1,32 +1,36 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import {expect} from 'chai'
+
+function assertDiagnosticExist(client:any, casePath:string, result:Object){
+    let diagnostic = client._diagnostics._data.get('file://' + casePath)
+    expect(diagnostic).to.be.not.empty
+    return expect(diagnostic[0]).to.contain.keys(result)
+}
+
 suite('test diagnostics', function() {
     test('should support single file error', function (){
-        const casesPath = path.join(__dirname, '..', '..', 'test', 'cases', 'single-file-diag.css')
+        const casePath = path.join(__dirname, '..', '..', 'test', 'cases', 'single-file-diag.css')
         const ext = vscode.extensions.getExtension('Wix.stylable-intelligence')
         let extClient:any;
-        let testDoc:vscode.TextDocument
 
         return ext!.activate()
                 .then(function(client) {
                     extClient = client
-                    return vscode.workspace.openTextDocument(casesPath)
+                    return vscode.workspace.openTextDocument(casePath)
                 })
                 .then(function(doc) {
-                    testDoc = doc;
                     return vscode.window.showTextDocument(doc)
                 })
                 .then(function(editor){
-                    let diagnostic = extClient._diagnostics._data.get('file://' + casesPath)
-                        expect(diagnostic[0]).to.include.keys({
+                    return assertDiagnosticExist(extClient, casePath, {
                         range:{
                             _start:{_line:1, _character:1},
                             _end: {_line:1, _character:13}
                         },
                         message:".root class cannot be used after spacing",
+                        severity: 0
                     })
-
                 })
     })
 })
