a5b1b96b3143e5555e8e9f3e6f81b2a021b88ecd
DoronTsur <doront@wix.com>
2017-08-23 18:04:10 +0300

    remove server

--- packages/client/server/src/utils/postcss-ast-utils.d.ts
+++
@@ -1,8 +0,0 @@
-import * as PostCss from 'postcss';
-import { ProviderPosition } from '../provider';
-export declare function isInNode(position: ProviderPosition, node: PostCss.NodeBase): boolean;
-export declare function isContainer(node: PostCss.NodeBase): node is PostCss.ContainerBase;
-export declare function isSelector(node: PostCss.NodeBase): node is PostCss.Rule;
-export declare function isDeclaration(node: PostCss.NodeBase): node is PostCss.Declaration;
-export declare function pathFromPosition(ast: PostCss.NodeBase, position: ProviderPosition, res?: PostCss.NodeBase[]): PostCss.NodeBase[];
-export declare function getPositionInSrc(src: string, position: ProviderPosition): number;
