93bffac092cb1bd8eaf28f2100d121f6a505bc6e
DoronTsur <doront@wix.com>
2017-08-27 01:33:01 +0300

    build-keep for production

---
+++ packages/client/build-keep.ts
@@ -0,0 +1,5 @@
+/*
+This is a place holder so while publush tsconfig will not flatten the src folder.
+It does it when it is the only one included. We accepet PRs.
+*/
+//# sourceMappingURL=build-keep.js.map
