a88d793e1a59d657903afcf1c358ea112d43032f
DoronTsur <doront@wix.com>
2017-08-27 01:48:14 +0300

    silly

--- packages/server/build-keep.ts
+++ packages/server/build-keep.ts
@@ -1,4 +1,4 @@
 /*
-This is a place holder so while publush tsconfig will not flatten the src folder.
+This is a place holder so while publishing, typescript will not flatten the src folder.
 It does it when it is the only one included. We accepet PRs.
 */
