8373c791b6a4e766909d11c079446858d2471e75
DoronTsur <doront@wix.com>
2017-08-29 13:11:11 +0300

    small changes to exposed classes

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,3 +1,3 @@
 export { MinimalDocs } from './minimal-docs'
 export {createProvider} from './provider-factory'
-export {default as Provider} from './provider'
+export {default as Provider, Completion, ProviderRange} from './provider'
