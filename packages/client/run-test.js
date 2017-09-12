6c097c712ac7695f1b4a9eb5caf299f4605629a4
Ory Henn <ory@wix.com>
2017-09-12 19:44:30 +0300

    test log

--- packages/client/run-test.js
+++ packages/client/run-test.js
@@ -1,4 +1,7 @@
+console.log('run-test')
 var path = require('path')
 process.env.CODE_TESTS_PATH = path.resolve(__dirname, './dist/test')
 process.env.CODE_TESTS_WORKSPACE = path.resolve(__dirname, './test/cases')
+console.log('require')
 require('./node_modules/vscode/bin/test')
+console.log('require done')
