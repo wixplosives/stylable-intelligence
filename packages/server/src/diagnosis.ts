a052227207ada1b43cd772f0c71b92a1060c9355
Ory Henn <ory@wix.com>
2017-09-25 13:03:22 +0300

    trying Windows again

--- packages/server/src/diagnosis.ts
+++ packages/server/src/diagnosis.ts
@@ -1,23 +1,29 @@
 import { StylableMeta } from 'stylable/dist/src/stylable-processor';
-import { Diagnostic, Range,DiagnosticSeverity } from 'vscode-languageserver-types/lib/main';
+import { Diagnostic, Range, DiagnosticSeverity } from 'vscode-languageserver-types/lib/main';
 import { TextDocument } from 'vscode-languageserver-types/lib/main';
 // import {NodeSource} from 'postcss'
 import * as path from 'path';
-import {safeParse, Diagnostics, process, StylableTransformer, FileProcessor} from 'stylable';
-import {Diagnostic as Report} from 'stylable/src/diagnostics'
+import { safeParse, Diagnostics, process as stylableProcess, StylableTransformer, FileProcessor } from 'stylable';
+import { Diagnostic as Report } from 'stylable/src/diagnostics'
 
-export function createDiagnosis(doc:TextDocument, fp:FileProcessor<StylableMeta>):Diagnostic[] {
-    let file = doc.uri.replace('file://','')
-    if (!doc.uri.endsWith('.st.css')) {return []};
+export function createDiagnosis(doc: TextDocument, fp: FileProcessor<StylableMeta>): Diagnostic[] {
+    let file: string;
+    if (process.platform === 'win32') {
+        file = doc.uri.replace('file:///', '')
+    } else {
+        file = doc.uri.replace('file://', '')
+    }
+
+    if (!doc.uri.endsWith('.st.css')) { return [] };
     let stylableDiagnostics = new Diagnostics()
     let transformer = new StylableTransformer({
         diagnostics: stylableDiagnostics,
         fileProcessor: fp,
-        requireModule: () => ({"default":{}})
+        requireModule: () => ({ "default": {} })
     })
 
-    let docPostCSSRoot = safeParse(doc.getText(), { from:path.resolve(file) })
-    let meta = process(docPostCSSRoot, stylableDiagnostics)
+    let docPostCSSRoot = safeParse(doc.getText(), { from: path.resolve(file) })
+    let meta = stylableProcess(docPostCSSRoot, stylableDiagnostics)
 
     fp.add(file, meta);
     transformer.transform(meta)
@@ -25,21 +31,21 @@ export function createDiagnosis(doc:TextDocument, fp:FileProcessor<StylableMeta>
 }
 
 //stylable diagnostic to protocol diagnostic
-function reportToDiagnostic(report:Report) {
-    let severity = report.type === 'error' ? DiagnosticSeverity.Error: DiagnosticSeverity.Warning
+function reportToDiagnostic(report: Report) {
+    let severity = report.type === 'error' ? DiagnosticSeverity.Error : DiagnosticSeverity.Warning
     let range = createRange(report)
-    return Diagnostic.create(range, report.message, severity )
+    return Diagnostic.create(range, report.message, severity)
 }
 
-function createRange(report:Report) {
+function createRange(report: Report) {
     let source = report.node.source
-    let start = {line: -1, character: -1}
-    let end =  {line: -1, character: -1}
-    if(report.options.word){
-        let lines:string[] = (source.input as any).css.split('\n')
+    let start = { line: -1, character: -1 }
+    let end = { line: -1, character: -1 }
+    if (report.options.word) {
+        let lines: string[] = (source.input as any).css.split('\n')
         const searchStart = source.start!.line - 1
-        const searchEnd =  source.end!.line -1
-        for (var i=searchStart; i < searchEnd; ++i) {
+        const searchEnd = source.end!.line - 1
+        for (var i = searchStart; i < searchEnd; ++i) {
             const wordIndex = lines[i].indexOf(report.options.word!)
             if (!!~wordIndex) {
                 start.line = i
@@ -50,9 +56,9 @@ function createRange(report:Report) {
             }
         }
     } else {
-        start.line =  source.start!.line - 1
-        start.character =source.start!.column - 1
-        end.line = source.end!.line -1
+        start.line = source.start!.line - 1
+        start.character = source.start!.column - 1
+        end.line = source.end!.line - 1
         end.character = source.end!.column
     }
     return Range.create(start, end)
