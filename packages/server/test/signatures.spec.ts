cae220c8e9e824f8ba38207a65cf0d99a8aba32b
Ory Henn <ory@wix.com>
2017-12-12 15:29:19 +0200

    js mixin tests

--- packages/server/test/signatures.spec.ts
+++ packages/server/test/signatures.spec.ts
@@ -54,32 +54,31 @@ describe('Signature Help', function () {
     });
 
     describe('JS Paramful Mixin', function () {
-        // let str = "'25','lala','b'";
+        let str = "'25','lala','b'";
 
-        // str.split('').forEach((c, i) => {
-        //     let prefix = str.slice(0, i);
-        //     it('Provides signature help and identifies active parameter, with prefix ' + prefix, function () {
-        //         let filePath = 'mixins/imported-mixins-paramful-signature.st.css'
+        str.split('').forEach((c, i) => {
+            let prefix = str.slice(0, i);
+            it('Provides signature help and identifies active parameter, with prefix ' + prefix, function () {
+                let filePath = 'mixins/imported-mixins-paramful-js-signature.st.css'
 
-        //         let sig = getSignatureHelp(filePath, prefix)
+                let sig = getSignatureHelp(filePath, prefix)
 
-        //         let exp: SignatureHelp = {
-        //             activeSignature: 0,
-        //             activeParameter: prefix.match(/,/g) ? prefix.match(/,/g)!.length : 0,
-        //             signatures: [SignatureInformation.create(
-        //                 "paramfulMixin(numParam: stNumber<0,200>, strParam: styl.stString, aliasedParam: lalaString, enumParam: 'a'|'b'):  styl.stCssFrag",
-        //                 undefined,
-        //                 ParameterInformation.create("numParam: stNumber<0,200>"),
-        //                 ParameterInformation.create("strParam: styl.stString"),
-        //                 ParameterInformation.create("aliasedParam: lalaString"),
-        //                 ParameterInformation.create("enumParam: 'a'|'b'"),
-        //             )]
-        //         }
+                let exp: SignatureHelp = {
+                    activeSignature: 0,
+                    activeParameter: prefix.match(/,/g) ? prefix.match(/,/g)!.length : 0,
+                    signatures: [SignatureInformation.create(
+                        "aMixin(strParam: stString, numParam: stNumber<0,200>, enumParam: 'a'|'b'): stCssFrag",
+                        "A mixin with some params",
+                        ParameterInformation.create("strParam: stString", "A string param" ),
+                        ParameterInformation.create("numParam: stNumber<0,200>", "A num param"),
+                        ParameterInformation.create("enumParam: 'a'|'b'", "An enum param"),
+                    )]
+                }
 
-        //         expect(sig).to.not.be.null;
-        //         expect(sig).to.deep.equal(exp)
-        //     }).timeout(5000);
-        // });
+                expect(sig).to.not.be.null;
+                expect(sig).to.deep.equal(exp)
+            }).timeout(5000);
+        });
     });
 
     describe('JS Paramless Mixin', function () {
