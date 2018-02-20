14e00f4ecdf5524e21195a07f993982eea99ced3
amir-arad <greenshade@gmail.com>
2018-02-20 09:42:41 +0200

    major structural changes move to single project

--- README.md
+++ README.md
@@ -7,14 +7,11 @@ Readme for the extension itself is at https://github.com/wix/stylable-intelligen
 
 # Project Setup
 * Clone this repo
-* Run yarn (in this order) in: packages/server/, packages/client/ and project root
-* To build server and copy to client: yarn start in packages/server/
-* To build server and run tests: yarn test in packages/server/
-* To run client tests from command line: yarn test in packages/client/
-  * To run client tests from command line, you must have no open instances of vscode. It is easier to run client tests from within vscode (see below).
-* To run client tests from vscode, open vscode in pakages/client/ and choose the 'Launch Tests' launch configuration.
-* To run server tests from vscode, open vscode in pakages/server/ and choose the 'run tests' launch configuration.
-* To launch and debug the extension:
+* Run `yarn` to install dependencies
+   * (investigate what's up with `postinstall` and `prepublish` hooks)
+* Run `yarn vscode:prepublish` ?? (needs to investigate further)
+* Run `yarn test` to test and build (both server and client)
+* To launch and debug the extension: (needs to investigate further)
   1. open vscode in packages/client/ and in packages/server/. 
   2. Choose the 'Launch Extension' configuration in the client vscode. 
   3. Once the extension is launched, choose the 'Attach' configuration in the server vscode. 
@@ -31,3 +28,55 @@ Readme for the extension itself is at https://github.com/wix/stylable-intelligen
   * Bug in completion text matching (mine): https://github.com/Microsoft/vscode/issues/34542
   * Lots of explanations and links to code about how completions are ordered and filtered: https://github.com/Microsoft/vscode/issues/26096
   * Link to activating completion ranking display: https://github.com/Microsoft/vscode/issues/41060#issuecomment-357879748
+
+
+# Client
+[![Build Status](https://travis-ci.org/wix/stylable-intelligence.svg?branch=master)](https://travis-ci.org/wix/stylable-intelligence)
+[![Build Status](https://ci.appveyor.com/api/projects/status/6ky876hm9nycyu8m/branch/master?svg=true)](https://ci.appveyor.com/project/qballer/stylable-intelligence)
+ 
+## Overview
+Stylable Intelligence is an extension implementing the Language Server Protocol that provides IDE support for Stylable.
+
+Stylable Intelligence is currently only supported in VSCode (version 1.18.0 and later). Support for JetBrains IDEs (WebStorm, IntelliJ) is planned.
+
+Currently supported: Code Completions, Diagnostics, Go to Definition, Syntax Highlighting
+
+All CSS language support functionality is also supported (hover hints, inline color picker, etc.). Some CSS diagnostics no longer apply in Stylable and are filtered out.
+
+Other Language Server features will be added in the near future.
+
+
+## Installation
+
+### From VSCode Marketplace
+Search VSCode Extension Marketplace for ['Stylable Intelligence'](https://marketplace.visualstudio.com/search?term=stylable-intelligence&target=VSCode&category=All%20categories&sortBy=Relevance).
+
+Install extension.
+
+Reload window when prompted by VsCode.
+
+### From .vsix file 
+In VsCode extension menu, choose 'Install from VSIX'.
+
+Double-click your .vsix file. 
+
+Reload window when prompted by VsCode.
+
+## Known Issues
+
+### Not supported yet (in rough order of priority):
+* Mixins and JS imports - WIP
+* Variants
+* Esoteric syntax (-st-compose, -st-theme, -st-root, -st-global)
+* Find References
+* Rename/Refactor
+
+# Server
+stylable-intelligence-server
+----------------------------
+stylable-intelligence completion provider to be consumed outside
+of the context of vscode extension.
+
+```typescript
+import {MinimalDocs, createProvider, Provider } from 'stylable-intelligence-server'
+```
---
+++ test/server/signatures.spec.ts
@@ -0,0 +1,205 @@
+import { expect } from 'chai'
+import { createProcessor } from '../../src/server/provider-factory'
+import { getSignatureHelp } from '../../test-kit/asserters'
+import { SignatureHelp, SignatureInformation, ParameterInformation } from 'vscode-languageserver';
+
+describe('Signature Help', function () {
+    describe('TS Paramful Mixin', function () {
+        let str = "'25','lala','b'";
+
+        str.split('').forEach((c, i) => {
+            let prefix = str.slice(0, i);
+            it('Provides signature help and identifies active parameter, with prefix ' + prefix, function () {
+                let filePath = 'mixins/imported-mixins-paramful-signature.st.css'
+
+                let sig = getSignatureHelp(filePath, prefix)
+
+                let exp: SignatureHelp = {
+                    activeSignature: 0,
+                    activeParameter: prefix.match(/,/g) ? prefix.match(/,/g)!.length : 0,
+                    signatures: [SignatureInformation.create(
+                        "paramfulMixin(numParam: stNumber<0, 200>, strParam: styl.stString, aliasedParam: lalaString, enumParam: 'a' | 'b'):  styl.stCssFrag",
+                        undefined,
+                        ParameterInformation.create("numParam: stNumber<0, 200>"),
+                        ParameterInformation.create("strParam: styl.stString"),
+                        ParameterInformation.create("aliasedParam: lalaString"),
+                        ParameterInformation.create("enumParam: 'a' | 'b'"),
+                    )]
+                }
+
+                expect(sig).to.not.be.null;
+                expect(sig).to.deep.equal(exp)
+            }).timeout(5000);
+        });
+    });
+
+    describe('TS Paramless Mixin', function () {
+        it('Provides signature help with no parameters', function () {
+            let filePath = 'mixins/imported-mixins-paramless-signature.st.css'
+
+            let sig = getSignatureHelp(filePath, '')
+
+            let exp: SignatureHelp = {
+                activeSignature: 0,
+                activeParameter: 0,
+                signatures: [SignatureInformation.create(
+                    "paramlessMixin():  styl.stCssFrag",
+                    undefined,
+                )]
+            }
+
+            expect(sig).to.not.be.null;
+            expect(sig).to.deep.equal(exp)
+        }).timeout(5000);
+    });
+
+    describe('TS Paramful Mixin - Default Import', function () {
+        let str = "'25','lala','b'";
+
+        str.split('').forEach((c, i) => {
+            let prefix = str.slice(0, i);
+            it('Provides signature help and identifies active parameter, with prefix ' + prefix, function () {
+                let filePath = 'mixins/imported-mixins-default-paramful-signature.st.css'
+
+                let sig = getSignatureHelp(filePath, prefix)
+
+                let exp: SignatureHelp = {
+                    activeSignature: 0,
+                    activeParameter: prefix.match(/,/g) ? prefix.match(/,/g)!.length : 0,
+                    signatures: [SignatureInformation.create(
+                        "mixin(pct: stPercent):  styl.stCssFrag",
+                        undefined,
+                        ParameterInformation.create("pct: stPercent"),
+                    )]
+                }
+
+                expect(sig).to.not.be.null;
+                expect(sig).to.deep.equal(exp)
+            }).timeout(5000);
+        });
+    });
+
+    describe('JS Paramful Mixin', function () {
+        let str = "'25','lala','b'";
+
+        str.split('').forEach((c, i) => {
+            let prefix = str.slice(0, i);
+            it('Provides signature help and identifies active parameter, with prefix ' + prefix, function () {
+                let filePath = 'mixins/imported-mixins-paramful-js-signature.st.css'
+
+                let sig = getSignatureHelp(filePath, prefix)
+
+                let exp: SignatureHelp = {
+                    activeSignature: 0,
+                    activeParameter: prefix.match(/,/g) ? prefix.match(/,/g)!.length : 0,
+                    signatures: [SignatureInformation.create(
+                        "aMixin(strParam: stString, numParam: stNumber<0,200>, enumParam: 'a'|'b'): stCssFrag",
+                        "A mixin with some params",
+                        ParameterInformation.create("strParam: stString", "A string param"),
+                        ParameterInformation.create("numParam: stNumber<0,200>", "A num param"),
+                        ParameterInformation.create("enumParam: 'a'|'b'", "An enum param"),
+                    )]
+                }
+
+                expect(sig).to.not.be.null;
+                expect(sig).to.deep.equal(exp)
+            }).timeout(5000);
+        });
+    });
+
+    describe('JS Paramless Mixin', function () {
+        it('Provides signature help with no parameters', function () {
+            let filePath = 'mixins/imported-mixins-paramless-js-signature.st.css'
+
+            let sig = getSignatureHelp(filePath, '')
+
+            let exp: SignatureHelp = {
+                activeSignature: 0,
+                activeParameter: 0,
+                signatures: [SignatureInformation.create(
+                    "aBareMixin(): stCssFrag",
+                    "A mixin with no params",
+                )]
+            }
+
+            expect(sig).to.not.be.null;
+            expect(sig).to.deep.equal(exp)
+        }).timeout(5000);
+    });
+
+    xdescribe('JS Paramful Mixin with .d.ts', function () {
+        let str = "'25','lala','b'";
+
+        str.split('').forEach((c, i) => {
+            let prefix = str.slice(0, i);
+            it('Provides signature help and identifies active parameter, with prefix ' + prefix, function () {
+                let filePath = 'mixins/imported-mixins-paramful-dts-signature.st.css'
+
+                let sig = getSignatureHelp(filePath, prefix)
+
+                let exp: SignatureHelp = {
+                    activeSignature: 0,
+                    activeParameter: prefix.match(/,/g) ? prefix.match(/,/g)!.length : 0,
+                    signatures: [SignatureInformation.create(
+                        "paramfulMixin(numParam: stNumber<0,200>, strParam: styl.stString, aliasedParam: lalaString, enumParam: 'a'|'b'):  styl.stCssFrag",
+                        undefined,
+                        ParameterInformation.create("numParam: stNumber<0,200>"),
+                        ParameterInformation.create("strParam: styl.stString"),
+                        ParameterInformation.create("aliasedParam: lalaString"),
+                        ParameterInformation.create("enumParam: 'a'|'b'"),
+                    )]
+                }
+
+                expect(sig).to.not.be.null;
+                expect(sig).to.deep.equal(exp)
+            }).timeout(5000);
+        });
+    });
+
+    xdescribe('JS Paramless Mixin with .d.ts', function () {
+        it('Provides signature help with no parameters', function () {
+            let filePath = 'mixins/imported-mixins-paramless-dts-signature.st.css'
+
+            let sig = getSignatureHelp(filePath, '')
+
+            let exp: SignatureHelp = {
+                activeSignature: 0,
+                activeParameter: 0,
+                signatures: [SignatureInformation.create(
+                    "paramlessMixin():  styl.stCssFrag",
+                    undefined,
+                )]
+            }
+
+            expect(sig).to.not.be.null;
+            expect(sig).to.deep.equal(exp)
+        }).timeout(5000);
+    });
+
+    describe('No signature', function () {
+        it('outside param area after parentheses', function () {
+            let filePath = 'mixins/imported-mixins-paramful-signature-outside-1.st.css'
+
+            let sig = getSignatureHelp(filePath, '')
+
+            expect(sig).to.be.null;
+        })
+
+        it('outside param area in mixin name', function () {
+            let filePath = 'mixins/imported-mixins-paramful-signature-outside-2.st.css'
+
+            let sig = getSignatureHelp(filePath, '')
+
+            expect(sig).to.be.null;
+        })
+
+        it('in value()', function () {
+            let filePath = 'variables/inside-value-local-vars.st.css'
+
+            let sig = getSignatureHelp(filePath, '')
+
+            expect(sig).to.be.null;
+        })
+    })
+});
+
