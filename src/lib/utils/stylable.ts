24dfa508cd77f7ae6e88d8ce0e7c05b1c94bdb98
amir-arad <greenshade@gmail.com>
2018-05-15 17:16:02 +0300

    also convert paths on import symbols in the meta

--- src/lib/utils/stylable.ts
+++ src/lib/utils/stylable.ts
@@ -1,5 +1,5 @@
 import * as path from "path";
-import {StylableMeta} from "stylable";
+import {ImportSymbol, StylableMeta, Imported, StylableSymbol} from "stylable";
 
 const rootPath = path.resolve('/');
 
@@ -7,10 +7,25 @@ export function fromStylablePath(stlPath: string) {
     return stlPath.replace(rootPath, '/').replace(/\\/g, '/');
 }
 
+function isImportSymbol(s: StylableSymbol): s is ImportSymbol {
+    return s && s._kind == 'import';
+}
+
+function normalizeImported(_import: Imported): Imported {
+    _import.from = fromStylablePath(_import.from);
+    _import.fromRelative = fromStylablePath(_import.fromRelative);
+    return _import;
+}
+
 export function normalizeMeta(meta: StylableMeta): StylableMeta {
     meta.source = fromStylablePath(meta.source);
-    meta.imports.forEach((_import) => {
-        _import.from = fromStylablePath(_import.from);
-    });
+    meta.imports.forEach(normalizeImported);
+
+    for (let name of Object.keys(meta.mappedSymbols)) {
+        const _symbol = meta.mappedSymbols[name];
+        if (isImportSymbol(_symbol)) {
+            normalizeImported(_symbol.import);
+        }
+    }
     return meta;
 }
