44695a6b8574ac97a61397e375db759c5b46abf2
amir-arad <greenshade@gmail.com>
2018-06-04 13:32:30 +0300

    fix vscodePath converters fix service test normalization of paths

--- src/lib/utils/uri-utils.ts
+++ src/lib/utils/uri-utils.ts
@@ -1,20 +1,20 @@
 
 const UriPrefix = 'file://';
 
-// TODO: convert implementation to `localPathToPosix(fromUrl(uri))` and fix all failing tests
+// TODO: convert implementation to `posixToLocalPath(fromUrl(uri))` and fix all failing tests
 export const fromVscodePath = (uri: string) => {
     if (uri.startsWith('file://')) {
-        return localPathToPosix(fromUrl(uri));
+        return posixToLocalPath(fromUrl(uri));
     }
     return uri;
 };
 
-// TODO: convert implementation to `toUrl(posixToLocalPath(path))` and fix all failing tests
+// TODO: convert implementation to `toUrl(localPathToPosix(path))` and fix all failing tests
 export const toVscodePath  = (path: string) => {
     if (path.startsWith('file://')) {
         return path;
     }
-    return toUrl(posixToLocalPath(path));
+    return toUrl(localPathToPosix(path));
 };
 
 export const fromUrl = (uri: string) => {
@@ -30,3 +30,16 @@ export const localPathToPosix  = (path: string) => {
 export const posixToLocalPath = (path: string): string => {
     return process.platform === 'win32' ?  path.slice(1).replace(/\//g, '\\') : path;
 };
+
+/*
+function isAbsoluteWn32(fp:string ) {
+    if (/[a-zA-Z]/.test(fp.charAt(0)) && fp.charAt(1) === ':' && fp.charAt(2) === '\\') {
+        return true;
+    }
+    // Microsoft Azure absolute filepath
+    if (fp.slice(0, 2) === '\\\\') {
+        return true;
+    }
+    return /^[\\\/]{2,}[^\\\/]+[\\\/]+[^\\\/]+/.test(fp) || /^([a-zA-Z]:)?[\\\/]/.test(fp);
+}
+*/
