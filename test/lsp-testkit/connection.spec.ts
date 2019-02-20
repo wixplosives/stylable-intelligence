4dd6d8e731c8ca38ffade692cf9b7824e9e9a1fe
Avi Vahl <avi.vahl@wix.com>
2019-02-21 01:23:27 +0200

    tsconfig: turn on noUnusedLocals

--- test/lsp-testkit/connection.spec.ts
+++ test/lsp-testkit/connection.spec.ts
@@ -28,7 +28,6 @@ import {
     CompletionItem,
     CompletionList,
     CompletionRequest,
-    Definition,
     DefinitionRequest,
     DidChangeConfigurationNotification,
     DidChangeConfigurationParams,
