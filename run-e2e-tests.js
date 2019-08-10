e5184b973721f59aac9012b5a570f521cb895fc7
Avi Vahl <avi.vahl@wix.com>
2019-08-11 02:26:33 +0300

    Infra: update test script to show error

--- run-e2e-tests.js
+++ run-e2e-tests.js
@@ -2,15 +2,17 @@ const path = require('path');
 const { runTests } = require('vscode-test');
 
 async function main() {
-    // Download VS Code, unzip it and run the integration test
-    await runTests({
-        extensionDevelopmentPath: __dirname,
-        extensionTestsPath: path.join(__dirname, 'dist/test/e2e'),
-        launchArgs: [path.join(__dirname, 'fixtures/e2e-cases')]
-    });
+    try {
+        // Download VS Code, unzip it and run the integration test
+        await runTests({
+            extensionDevelopmentPath: __dirname,
+            extensionTestsPath: path.join(__dirname, 'dist/test/e2e'),
+            launchArgs: [path.join(__dirname, 'fixtures/e2e-cases')]
+        });
+    } catch (e) {
+        console.error('Test Failed', e);
+        process.exit(1);
+    }
 }
 
-main().catch(() => {
-    console.error('Failed to run tests');
-    process.exit(1);
-});
+main();
