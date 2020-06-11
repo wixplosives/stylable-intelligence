531e0994e27b8b245d3a5bc2252712cbd63c7e4d
Avi Vahl <avi.vahl@wix.com>
2020-06-11 03:23:11 +0300

    test: ensure local extensions don't load the notification from my local vscode-icons got the e2e stuck using the latest vscode.

--- run-e2e-tests.js
+++ run-e2e-tests.js
@@ -5,8 +5,8 @@ const { runTests } = require('vscode-test');
 runTests({
     extensionDevelopmentPath: __dirname,
     extensionTestsPath: path.join(__dirname, 'dist/test/e2e'),
-    launchArgs: [path.join(__dirname, 'fixtures/e2e-cases')]
-}).catch(e => {
+    launchArgs: ['--disable-extensions', path.join(__dirname, 'fixtures/e2e-cases')],
+}).catch((e) => {
     console.error('Test Failed', e);
     process.exit(1);
 });
