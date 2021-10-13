ab54878a4fc82e2bc42bb569a41732c71bd75959
Tom Raviv <tomr@wix.com>
2021-10-13 15:38:48 +0300

    improve e2e typings

--- test/e2e/diagnostics.test.ts
+++ test/e2e/diagnostics.test.ts
@@ -1,16 +1,14 @@
 import fs from '@file-services/node';
-import vscode, { TextDocument } from 'vscode';
+import vscode, { Diagnostic, Range, TextDocument } from 'vscode';
 import path from 'path';
 import { expect } from 'chai';
+import { LanguageClient } from 'vscode-languageclient/node';
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-function assertDiagnosticExist(client: any, doc: TextDocument, result: object) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    const diagnostics = client.diagnostics.get(doc.uri);
+function assertDiagnosticExist(client: LanguageClient, doc: TextDocument, result: Diagnostic) {
+    const diagnostics = client.diagnostics!.get(doc.uri);
 
     expect(diagnostics).to.have.length.greaterThan(0);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    return expect(diagnostics[0]).to.contain.keys(result);
+    return expect(diagnostics![0]).to.contain.keys(result);
 }
 
 suite('test diagnostics', function () {
@@ -23,18 +21,14 @@ suite('test diagnostics', function () {
 
     test('should support single file error', async () => {
         const casePath = path.join(rootDir, 'fixtures', 'e2e-cases', 'single-file-diag.st.css');
-        const ext = vscode.extensions.getExtension('wix.stylable-intelligence');
+        const ext = vscode.extensions.getExtension<LanguageClient>('wix.stylable-intelligence');
 
         if (ext) {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
             const client = await ext.activate();
             const doc = await vscode.workspace.openTextDocument(casePath);
             await vscode.window.showTextDocument(doc);
             return assertDiagnosticExist(client, doc, {
-                range: {
-                    _start: { _line: 1, _character: 1 },
-                    _end: { _line: 1, _character: 13 },
-                },
+                range: new Range(1, 1, 1, 13),
                 message: '.root class cannot be used after spacing',
                 severity: 0,
             });
