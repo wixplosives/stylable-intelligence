cd49f38848e50fffd9dca1043bb97d6bb8996492
Tom Raviv <tomr@wix.com>
2021-10-13 14:43:26 +0300

    fix(e2e): refactor to support vscode internal api change

--- test/e2e/diagnostics.test.ts
+++ test/e2e/diagnostics.test.ts
@@ -1,30 +1,16 @@
 import fs from '@file-services/node';
-import vscode from 'vscode';
+import vscode, { TextDocument } from 'vscode';
 import path from 'path';
 import { expect } from 'chai';
 
-// const fileUriToNativePath = (uri: string) => isWindows ? uri.slice(8).replace('%3A', ':') : uri.slice(7);
-
-function getPathToDiagnostics(casePath: string) {
-    let pathToFile = '';
-    if (process.platform === 'win32') {
-        pathToFile = casePath.split('\\').join('/');
-        pathToFile = pathToFile.split(':').join('%3A');
-        pathToFile = pathToFile.charAt(0).toLowerCase() + pathToFile.slice(1);
-        pathToFile = 'file:///' + pathToFile;
-    } else {
-        pathToFile = 'file://' + casePath;
-    }
-    return pathToFile;
-}
-
 // eslint-disable-next-line @typescript-eslint/ban-types
-function assertDiagnosticExist(client: any, casePath: string, result: object) {
-    // eslint-disable-next-line
-    const diagnostic = client._diagnostics._data.get(getPathToDiagnostics(casePath));
-    expect(diagnostic).to.have.length.greaterThan(0);
+function assertDiagnosticExist(client: any, doc: TextDocument, result: object) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    const diagnostics = client.diagnostics.get(doc.uri);
+
+    expect(diagnostics).to.have.length.greaterThan(0);
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    return expect(diagnostic[0]).to.contain.keys(result);
+    return expect(diagnostics[0]).to.contain.keys(result);
 }
 
 suite('test diagnostics', function () {
@@ -44,7 +30,7 @@ suite('test diagnostics', function () {
             const client = await ext.activate();
             const doc = await vscode.workspace.openTextDocument(casePath);
             await vscode.window.showTextDocument(doc);
-            return assertDiagnosticExist(client, casePath, {
+            return assertDiagnosticExist(client, doc, {
                 range: {
                     _start: { _line: 1, _character: 1 },
                     _end: { _line: 1, _character: 13 },
