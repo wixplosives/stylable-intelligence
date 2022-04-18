2d9ec657b1ddc9575548f6cb0db71933371e3a91
Tom Raviv <tomr@wix.com>
2022-04-18 14:42:25 +0300

    chore: migrate to @vscode/test-electron from vscode-test

--- run-e2e-tests.js
+++ run-e2e-tests.js
@@ -1,12 +1,22 @@
 const path = require('path');
-const { runTests } = require('vscode-test');
+const { runTests } = require('@vscode/test-electron');
 
-// Download VS Code, unzip it and run the integration test
-runTests({
-    extensionDevelopmentPath: __dirname,
-    extensionTestsPath: path.join(__dirname, 'dist/test/e2e'),
-    launchArgs: ['--disable-extensions', path.join(__dirname, 'fixtures/e2e-cases')],
-}).catch((e) => {
-    console.error('Test Failed', e);
-    process.exit(1);
-});
+async function main() {
+    try {
+        // The folder containing the Extension Manifest package.json
+        // Passed to `--extensionDevelopmentPath`
+        const extensionDevelopmentPath = __dirname;
+
+        // The path to the extension test script
+        // Passed to --extensionTestsPath
+        const extensionTestsPath = path.join(__dirname, './dist/test/e2e/index');
+
+        // Download VS Code, unzip it and run the integration test
+        await runTests({ extensionDevelopmentPath, extensionTestsPath });
+    } catch (err) {
+        console.error('Failed to run tests');
+        process.exit(1);
+    }
+}
+
+main();
