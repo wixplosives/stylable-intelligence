b6f8cedbbfcb639ca919c024040bde905af7bfa1
Ido Rosenthal <idoros@gmail.com>
2023-02-15 10:00:27 +0200

    chore: removed unused code

--- src/lib/normalize-config.ts
+++ src/lib/normalize-config.ts
@@ -13,18 +13,3 @@ export interface VSCodeStylableExtensionConfig {
         wrapLineLength: number;
     };
 }
-
-export function normalizeConfig(config: VSCodeStylableExtensionConfig): ExtensionConfiguration {
-    const { diagnostics, formatting } = config;
-    const { endOfLine, endWithNewLine, newLineBetweenRulesets, newLineBetweenSelectors } = formatting;
-
-    return {
-        diagnostics: diagnostics,
-        formatting: {
-            selector_separator_newline: newLineBetweenSelectors,
-            newline_between_rules: newLineBetweenRulesets,
-            end_with_newline: endWithNewLine,
-            eol: endOfLine,
-        },
-    };
-}
