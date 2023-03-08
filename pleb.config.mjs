b36ce0f49a53b68a6a19ba2581366f409aa2669b
Ido Rosenthal <idoros@gmail.com>
2023-03-08 09:35:09 +0200

    chore: upgrade stylable dependencies

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -1,6 +1,3 @@
 export default {
-  pinnedPackages: [
-    { name: '@types/vscode', reason: 'minimal supported version of vscode api' },
-    { name: 'rimraf', reason: 'drop support for glob' },
-  ],
+  pinnedPackages: [{ name: '@types/vscode', reason: 'minimal supported version of vscode api' }],
 };
