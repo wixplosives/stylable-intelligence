fd939f20e98fe94b839f7b328b9e022cdfcd0e7d
Ido Rosenthal <idoros@gmail.com>
2024-05-22 11:31:34 +0300

    chore: upgrade dependencies
    - kept stylable and file-services upgrade with old version in order to deal with stylable upgrade separately

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -5,5 +5,6 @@ export default {
       name: 'chai',
       reason: 'esm only from v5',
     },
+    { name: 'eslint', reason: 'v9 has breaking changes that require changes from all plugins' },
   ],
 };
